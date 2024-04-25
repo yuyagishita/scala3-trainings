e6e61534327ee003f481989efb284778704068ca
yuyagishita <yu.yagishita1995@gmail.com>
2024-04-25 23:51:31 +0900

    Opaque Type

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -9,6 +9,7 @@ import scala.collection.mutable.ListBuffer
   println(oldNumbers)
   println(newNumbers)
   println(scalaNewNubmers)
+  println(Logarithm(1)*(Logarithm(2)))
 
 def msg = "I was compiled by Scala 3. :)"
 
@@ -28,3 +29,19 @@ trait Animal {
 }
 trait Animal2:
   def hoge: Unit
+
+
+opaque type Logarithm = Double
+
+object Logarithm:
+  def apply(d: Double): Logarithm = math.log(d)
+  def safe(d: Double): Option[Logarithm] = 
+    if d > 0.0 then Some(math.log(d)) else None
+end Logarithm
+
+extension (x: Logarithm)
+  def toDouble: Double = math.exp(x)
+  def + (y: Logarithm): Logarithm = Logarithm(math.exp(x) + math.exp(y))
+  def * (y: Logarithm): Logarithm = x + y
+
+
